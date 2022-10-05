70924da8870a0d1d87176437ed1ad3b779d194b5
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-10-05 20:35:15 +0530

    include headers

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from app.models import UserList
 import flask
 from flask import Request
 
+headers = {
+    "Content-Type": "application/json",
+}
+
 
 def getAllUsers(request: Request):
     users = get_all_users()
-    return UserList(__root__=users).to_json()
+    return (UserList(__root__=users).to_json(), 200, headers)
 
 
 if __name__ == "__main__":
