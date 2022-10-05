7d66042e8cf642debfd8f46fcd8043b423f24331
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-10-05 16:43:28 +0530

    Add cloud func file

---
+++ scripts/deploy_func.sh
@@ -0,0 +1,5 @@
+
+PROJECT_ID=$(gcloud config get-value project)
+gcloud builds submit --config cloudbuild_gcf.yaml \
+   --project $PROJECT_ID --no-source \
+    --substitutions=_GIT_SOURCE_BRANCH="master",_GIT_SOURCE_URL="https://github.com/AksAman/gcp-deploy",_GIT_REPO_NAME="gcp-deploy"
