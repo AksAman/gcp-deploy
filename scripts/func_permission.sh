696a535f533d9e2e24ac53166a9e0b053b75bed2
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-10-05 18:18:18 +0530

    update scripts

--- scripts/func_permission.sh
+++ scripts/func_permission.sh
@@ -1,4 +1,2 @@
 FUNCTION_NAME=gogcp-all
-
-CF_TRIGGER_URL="https://us-central1-learninggcp-364602.cloudfunctions.net/gogcp-all"
 gcloud alpha functions add-iam-policy-binding $FUNCTION_NAME --member=allUsers --role=roles/cloudfunctions.invoker
--- scripts/loadgen_func.sh
+++ scripts/loadgen_func.sh
@@ -1,9 +1,7 @@
 REGION=us-central1
 FUNCTION_NAME=gogcp-all
-# export CLOUD_RUN_SERVICE_URL=$(gcloud run services describe $PROJECT_NAME --region $REGION --format 'value(status.url)')
-# export CF_TRIGGER_URL=$(gcloud functions describe $FUNCTION_NAME --format="value(httpsTrigger.url)")
+export CF_TRIGGER_URL=$(gcloud functions describe $FUNCTION_NAME --format="value(httpsTrigger.url)")
 
-CF_TRIGGER_URL="https://us-central1-learninggcp-364602.cloudfunctions.net/gogcp-all"
 echo $CF_TRIGGER_URL
 
 # curl -s $CF_TRIGGER_URL
