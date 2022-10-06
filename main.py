ecd8e46ec0568ac3068cce78e63546b14c37aa7f
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-10-06 14:25:55 +0530

    add logging to function

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import logging
 from app.router import get_all_users
 from app.models import UserList
-import flask
-from flask import Request
+
+logger = logging.getLogger(__name__)
+
 
 headers = {
     "Content-Type": "application/json",
 }
 
 
-def getAllUsers(request: Request):
+def getAllUsers(request):
+    logger.info(f"{type(request)=}")
+    logger.info(f"{dir(request)=}")
     users = get_all_users()
     return (UserList(__root__=users).to_json(), 200, headers)
 
