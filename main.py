e3af4c8456f49d178f90ef6ed2524ab03dd1ae20
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-10-06 16:10:57 +0530

    add cached in return

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ from app.models import UserList
 from app.router import get_all_users
 
 
+cached_data = {}
+
+
 @functions_framework.http
 def getAllUsers(request):
+    KEY = "ALL_USERS"
+    if KEY in cached_data:
+        return cached_data[KEY]
     users = get_all_users()
-    return Response(
+    response = Response(
         response=UserList(__root__=users).json(),
         status=200,
         content_type="application/json",
     )
+    cached_data[KEY] = response
+    return cached_data[KEY]
 
 
 if __name__ == "__main__":
