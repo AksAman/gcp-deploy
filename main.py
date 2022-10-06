20425ffba2d126a07b6c0d0965f8c6263b0bc997
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-10-06 14:46:31 +0530

    add functions_framework

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from app.router import get_all_users
-from app.models import UserList
-from google.cloud import logging as cloudlogging
 import logging
 
-lg_client = cloudlogging.Client()
-lg_client.setup_logging(log_level=logging.INFO)
+import functions_framework
+
+# from google.cloud import logging as cloudlogging
+
+from app.models import UserList
+from app.router import get_all_users
+
+# lg_client = cloudlogging.Client()
+# lg_client.setup_logging(log_level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 headers = {
@@ -12,11 +16,10 @@ headers = {
 }
 
 
+@functions_framework.http
 def getAllUsers(request):
     logger.info(f"CUSTOM LOG: {type(request)=}")
     logger.info(f"CUSTOM LOG: {dir(request)=}")
-    print(f"CUSTOM PRINT: {type(request)=}")
-    print(f"CUSTOM PRINT: {dir(request)=}")
     users = get_all_users()
     return (UserList(__root__=users).to_json(), 200, headers)
 
