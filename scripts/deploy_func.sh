2bae567bc4fc74b8c0e3b3fddb3991a40c4e4762
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-10-06 15:14:08 +0530

    region-mumbai

--- scripts/deploy_func.sh
+++ scripts/deploy_func.sh
@@ -1,5 +1,8 @@
 
 PROJECT_ID=$(gcloud config get-value project)
+# REGION=us-central1
+REGION=asia-south1
+
 gcloud builds submit --config cloudbuild_gcf.yaml \
    --project $PROJECT_ID --no-source \
-    --substitutions=_GIT_SOURCE_BRANCH="python",_GIT_SOURCE_URL="https://github.com/AksAman/gcp-deploy",_GIT_REPO_NAME="gcp-deploy"
+    --substitutions=_GIT_SOURCE_BRANCH="python",_GIT_SOURCE_URL="https://github.com/AksAman/gcp-deploy",_GIT_REPO_NAME="gcp-deploy",REGION=$REGION
